d48c778586f331662bee87ba713641a6f03146a0
kick250 <kicklobat@gmail.com>
2023-10-12 12:38:08 -0300

    criar contagem de produtos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './index.module.css';
+import { useSelector } from 'react-redux';
 
 
 export default function Header() {
+  const products = useSelector(store => store.products)
+
   return (
     <header className={styles.header}>
       <h1>Produtos</h1>
@@ -10,6 +13,9 @@ export default function Header() {
         <Link to='/new'>Criar produtos</Link>
         <Link to='/products'>Ver produtos</Link>
       </div>
+      <span>
+        Total de produtos: {products.length}
+      </span>
     </header>
   )
 }
